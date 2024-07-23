c4e8e72b1d799ea41656ff9b22e267ec04f3f017
Mandar Tule <mandartule5@gmail.com>
2024-07-23 19:39:41 +0530

    backend url changed

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3333';
-//export const API_URL = 'https://todo-backend-nest.onrender.com';
+//export const API_URL = 'http://localhost:3333';
+export const API_URL = 'https://todo-backend-nest.onrender.com';
